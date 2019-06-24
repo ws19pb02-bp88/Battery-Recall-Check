bb20f7bc8f4ddf52566ad6367fafc3e87f29733c
BP88 <balmes.pavlov@gmail.com>
2019-06-24 08:37:42 -0400

    Update 2015_MBP_15-inch_Battery_Recall_Script_Check.py

--- 2015_MBP_15-inch_Battery_Recall_Script_Check.py
+++ 2015_MBP_15-inch_Battery_Recall_Script_Check.py
@@ -1,3 +1,11 @@
+"""
+https://support.apple.com/15-inch-macbook-pro-battery-recall
+Check a CSV containing the serial numbers of MacBook Pros against the
+Apple Battery Recall.
+The CSV must contain two columns: "Serial Number" and "Computer Name".
+"""
+
+
 import requests
 import json
 import uuid
@@ -14,42 +22,26 @@ with open(file) as csv_file:
         guid = str(uuid.uuid4())
         data = json.dumps({'serial': serial, 'GUID': guid})
         headers = {'content-type': 'application/json'}
-        request = requests.post(url = 'https://qualityprograms.apple.com/snlookup/062019', data = data, headers = headers)
+        request = requests.post(url='https://qualityprograms.apple.com/snlookup/062019', data=data, headers=headers)
         response = json.loads(request.text)
         if response["status"] == "E00":
-            dict = {"Serial": serial,
-                    "Computer_Name": row["Computer Name"],
-                    "Status": "Eligible"
-                    }
-            computer_list.append(dict)
+            status = "Eligible"
         elif response["status"] == "E01":
-            dict = {"Serial": serial,
-                    "Computer_Name": row["Computer Name"],
-                    "Status": "Ineligible"
-                    }
-            computer_list.append(dict)
+            status = "Ineligible"
         elif response["status"] == "E99":
-            dict = {"Serial": serial,
-                    "Computer_Name": row["Computer Name"],
-                    "Status": "Web Error"
-                    }
-            computer_list.append(dict)
+            status = "Web Error"
         elif response["status"] == "FE01":
-            dict = {"Serial": None,
-                    "Computer_Name": row["Computer Name"],
-                    "Status": "Empty Serial"
-                    }
-            computer_list.append(dict)
+            status = "Empty Serial"
         elif response["status"] == "FE03":
-            dict = {"Serial": serial,
-                    "Computer_Name": row["Computer Name"],
-                    "Status": "Invalid Serial"
-                    }
-            computer_list.append(dict)
+            status = "Invalid Serial"
+        dict = {"Serial": serial,
+                "Computer_Name": row["Computer Name"],
+                "Status": status
+                }
+        computer_list.append(dict)
 
 for computer in computer_list:
     if computer["Status"] == "Eligible":
         print("Computer Name: {}").format(computer["Computer_Name"])
         print("Serial Number: {}").format(computer["Serial"])
         print("===================")
-
